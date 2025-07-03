e1d1480f54dd1e5d7265172843e13a4cda0328d9
waleed smadi <waleed0smadi@outlook.com>
2025-07-03 22:00:28 +0300

    create tickets models
     1 - create Movie model
    
     2 - create Guest model
    
     3 - create Reservation model
    
     4 - install phonenumber library
    
     5 - makemigrations & migrate

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+class Movie(models.Model):
+    hall = models.CharField(max_length=10)
+    movie = models.CharField(max_length=10)
+    date = models.DateField()
+
+
+class Guest(models.Model):
+    name = models.CharField(max_length=30)
+    mobile = PhoneNumberField(null=False, blank=False, unique=True)
+
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE, related_name='reservation')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reservation')
